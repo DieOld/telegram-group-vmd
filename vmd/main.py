e728864bbea8156e578981d7696169d274f52ae8
DieOld <vadym.tishchenko2@gmail.com>
2021-01-09 15:52:54 +0200

    Add video note filter

--- vmd/main.py
+++ vmd/main.py
@@ -19,6 +19,6 @@ logger = logging.getLogger(__name__)
 if __name__ == '__main__':
     updater = Updater(token=os.environ['TELEGRAM_BOT_TOKEN'], use_context=True)
     dispatcher = updater.dispatcher
-    dispatcher.add_handler(MessageHandler(Filters.voice, handlers.voice_message_handler))
+    dispatcher.add_handler(MessageHandler(Filters.voice | Filters.video_note, handlers.voice_message_handler))
     updater.start_polling()
     updater.idle()
